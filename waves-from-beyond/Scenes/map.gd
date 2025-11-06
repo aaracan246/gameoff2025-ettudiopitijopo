f73599ab4cbd65dba0712038ebd0dd5e42d43cfd
smorcor744 <148891340+smorcor744@users.noreply.github.com>
2025-11-06 12:56:50 +0100

    Refactor unused parameters in _on_radio_input_event
    Renamed unused parameters in _on_radio_input_event to use underscores in map.gd and player.gd, following Godot best practices and clarifying intent.

--- waves-from-beyond/Scenes/map.gd
+++ waves-from-beyond/Scenes/map.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 
-func _on_radio_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+func _on_radio_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_mask == 1:
 		$Player.current = false
 		$Room/Radio/Camera3D.current = true
