5111b03d52b46e7757f5c2484cfd1c95254bbfe7
Samuel <samuelurbinaflor@gmail.com>
2025-11-07 20:17:31 +0100

    pause_menu_finished

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var continue_btn: Button = $VBoxContainer/continue
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+	
+
+
+
+func _on_continue_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/demo.tscn")
---
+++ Scripts/UI/pause_menu.gd
@@ -0,0 +1,49 @@
+extends Control
+@onready var pause_menu: Control = $"."
+
+@onready var resume_btn: Button = $VBoxContainer/resume_btn
+@onready var settings_btn: Button = $VBoxContainer/settings_btn
+@onready var exit_btn: Button = $VBoxContainer/exit_btn
+
+@onready var confirm_exit: ConfirmationDialog = $confirm_exit
+@onready var settings: Control = $settings
+
+func _ready() -> void:
+	confirm_exit.visible = false
+	settings.visible = false
+	
+# Se llama desde el CanvasLayer
+func toggle_pause():
+	if get_tree().paused:
+		resume_game()
+	else:
+		pause_game()
+
+
+func pause_game():
+	get_tree().paused = true
+	pause_menu.visible = true
+	
+func resume_game():
+	get_tree().paused = false
+	pause_menu.visible = false
+	settings.visible = false
+	
+	
+# Main buttons
+func _on_resume_btn_pressed() -> void:
+	resume_game()
+
+func _on_settings_btn_pressed() -> void:
+	settings.visible = true
+	
+func _on_exit_btn_pressed() -> void:
+	confirm_exit.visible = true
+
+# confirm exit	
+func _on_confirm_exit_confirmed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/UI/main_menu.tscn")
+
+func _on_confirm_exit_canceled() -> void:
+	confirm_exit.visible = false
