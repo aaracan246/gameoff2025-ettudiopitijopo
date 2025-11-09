3dc856265f93bc614a739e996551b3b7da657582
smorcor744 <148891340+smorcor744@users.noreply.github.com>
2025-11-09 15:13:47 +0100

    Add computer screen interaction and custom cursor

---
+++ Scripts/computer.gd
@@ -0,0 +1,55 @@
+extends StaticBody3D
+@onready var screen: MeshInstance3D = $Screen
+@onready var sub_viewport: SubViewport = $Screen/SubViewport
+@onready var area_3d: Area3D = $Screen/Area3D
+
+var mouse_inside = false
+var selected = false
+func _ready() -> void:
+	#area_3d.mouse_entered.connect()
+	input_event.connect(_on_input_event)
+	
+func _on_input_event(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int):
+	var mouse3D = screen.global_transform.affine_inverse() * event_position
+	var calculate2DPosition = Vector2(mouse3D.x,mouse3D.z)
+	
+	var screenSize = screen.mesh.size
+	calculate2DPosition += screenSize / 2
+	calculate2DPosition /= screenSize
+	
+	var mouse2D = calculate2DPosition * Vector2(sub_viewport.size)
+	
+	event.position = mouse2D
+	
+	sub_viewport.push_input(event)
+	
+func set_select(boolean: bool):
+	selected = boolean
+	
+	if boolean:
+		Input.mouse_mode = Input. MOUSE_MODE_VISIBLE
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _on_mouse_input_event(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	var mouse3D = screen.global_transform.affine_inverse() * event_position
+	var calculate2DPosition = Vector2(mouse3D.x,mouse3D.z)
+	
+	var screenSize = screen.mesh.size
+	calculate2DPosition += screenSize / 2
+	calculate2DPosition /= screenSize
+	
+	var mouse2D = calculate2DPosition * Vector2(sub_viewport.size)
+	
+	event.position = mouse2D
+	
+	sub_viewport.push_input(event)
+
+
+func _on_mouse_entered() -> void:
+	mouse_inside = true
+
+
+func _on_mouse_exited() -> void:
+	mouse_inside = false
--- Scripts/screem.gd
+++ Scripts/screem.gd
@@ -5,12 +5,14 @@ var camera:Camera3D
 @onready var subviewport = $Camera/SubViewport
 @onready var destktop = $Destktop
 @onready var Outdoor_camera = $Camera
+@onready var camera_button: Button = $Destktop/camera
+@onready var exit: Button = $Camera/exit
+
 
 func _ready() -> void:
 	camera = get_tree().get_root().get_node("Demo/Room/OutDoor")
 	new_camera.global_transform = camera.global_transform
-	print(camera)
-	print(camera is Camera3D)  
+ 
 
 	
 func _on_camera_pressed() -> void:
@@ -21,3 +23,23 @@ func _on_camera_pressed() -> void:
 func _on_exit_pressed() -> void:
 	destktop.visible = true
 	Outdoor_camera.visible = false
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			
+			match event.global_position:
+				camera_button.global_position:
+					camera_button.emit_signal("pressed")
+					print("PRESIONADOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+
+
+func _on_mouse_entered() -> void:
+	var custom_cursor_image = load("res://icon.svg")
+	Input.set_custom_mouse_cursor(custom_cursor_image) 
+
+
+func _on_mouse_exited() -> void:
+	var custom_cursor_image = load("res://cursor.png")
+	Input.set_custom_mouse_cursor(custom_cursor_image) 
