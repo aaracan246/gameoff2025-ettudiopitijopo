8512a3f2dbd93938c7998b097577ba6b1194d8f7
lnarote777 <nolara@iesrafaelalberti.es>
2025-11-21 11:55:51 +0100

    credits

--- Scripts/UI/settings.gd
+++ Scripts/UI/settings.gd
@@ -6,6 +6,8 @@ signal vhs_filter_changed(value:bool)
 @onready var check = $PanelContainer/MarginContainer/Container/HBoxContainer/VHS/CheckButton
 @onready var close: Button = $PanelContainer/MarginContainer/Container/Header/close
 
+@onready var audio = get_node("/root/AudioManager")
+
 
 func _ready() -> void:
 	check.button_pressed = Global.vhs_enabled
@@ -17,7 +19,3 @@ func _on_close_pressed() -> void:
 func _on_check_button_pressed() -> void:
 	Global.vhs_enabled = check.button_pressed
 	emit_signal("vhs_filter_changed", check.button_pressed)
-
-
-func _on_master_btn_pressed() -> void:
-	pass # Replace with function body.
