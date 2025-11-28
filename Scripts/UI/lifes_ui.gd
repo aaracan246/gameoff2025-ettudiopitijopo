f5d16321234d0ff8814ea32137f579cc1af13467
lnarote777 <nolara@iesrafaelalberti.es>
2025-11-28 18:15:24 +0100

    alberto me obligo a pushear :(

--- Scripts/UI/lifes_ui.gd
+++ Scripts/UI/lifes_ui.gd
@@ -3,7 +3,6 @@ extends Control
 
 @onready var life_lost_1: Sprite2D = $MarginContainer/BoxContainer/HBoxContainer/life_1
 @onready var life_lost_2: Sprite2D = $MarginContainer/BoxContainer/HBoxContainer/life_2
-
 const LIFE_LOST = preload("uid://bcss78ks18dp4")
 
 
